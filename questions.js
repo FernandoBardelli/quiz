3fd502c1a23bda4f7019e2d793be3c349f427b92
Fernando Bardelli <fernandofelipebardelli@gmail.com>
2022-10-18 22:00:11 -0300

    primeiro commit, adicionando arquivos ao repositório remoto

---
+++ questions.js
@@ -0,0 +1,134 @@
+let questions = [
+    {
+        question: 'Qual foi a primeira seleção a conquistar a Copa do Mundo?',
+        options: [
+            'Argentina',
+            'Uruguai',
+            'Brasil',
+            'Alemanha',
+            'França',
+        ],
+        answer: 1
+    },
+    {
+        question: 'Em que ano aconteceu a primeira copa do mundo e qual foi o país sede?',
+        options: [
+            '1934 - Itália',
+            '1950 - Brasil',
+            '1930 - Uruguai',
+            '1938 - França',
+            '1954 - Suiça',
+        ],
+        answer: 2
+    },
+    {
+        question: 'Quem é o maior artilheiro da Copa do Mundo? ',
+        options: [
+            'Pelé (Brasil)',
+            'Gerd Müller (Alemanha Ocidental)',
+            'Just Fontaine (França)',
+            'Miroslav Klose (Alemanha)',
+            'Ronaldo Fenômeno (Brasil)',
+        ],
+        answer: 3
+    },
+    {
+        question: 'Em que ano a seleção brasileira conquistou sua primeira taça?',
+        options: [
+            '1950',
+            '1958',
+            '1930',
+            '1962',
+            '1966',
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual a maior goleada que já aconteceu em uma edição de Copa do Mundo?',
+        options: [
+            'Brasil 1 x 7 Alemanha',
+            'Portugal 7 x 0 Coreia do Norte',
+            'Alemanha 8 x 0 Arábia Saudita',
+            'Iugoslávia 9 x 0 Zaire',
+            'Hungria 10 x 1 El Salvador',
+        ],
+        answer: 4
+    },
+    {
+        question: 'Quem foi o jogador mais jovem a ganhar uma Copa do Mundo?',
+        options: [
+            'Kylian Mbappé (França)',
+            'Kaká (Brasil)',
+            'Pelé (Brasil)',
+            'Giuseppe Bergomi (Itália)',
+            'Ronaldo Fenômeno (Brasil)',
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual o país que mais participou de edições da Copa do Mundo?',
+        options: [
+            'Alemanha',
+            'Uruguai',
+            'Argentina',
+            'Brasil',
+            'Itália',
+        ],
+        answer: 3
+    },
+    {
+        question: 'Qual país chegou mais vezes à final da Copa do Mundo? ',
+        options: [
+            'Brasil',
+            'Alemanha',
+            'Itália',
+            'Argentina',
+            'Uruguai',
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual país foi mais vezes vice-campeão (2º Lugar) da Copa do Mundo?',
+        options: [
+            'Holanda',
+            'Croácia',
+            'Brasil',
+            'França',
+            'Alemanha',
+        ],
+        answer: 4
+    },
+    {
+        question: 'Qual é o jogador que tem mais partidas em Copas do Mundo?',
+        options: [
+            'Miroslav Klose (Alemanha)',
+            'Paolo Maldini (Itália)',
+            'Diego Maradona (Argentina)',
+            'Lothar Matthäus (Alemanha)',
+            'Cafu (Brasil)',
+        ],
+        answer: 3
+    },
+    {
+        question: 'Quem foi o maior artilheiro de uma mesma edição da Copa do Mundo?',
+        options: [
+            'Pelé (Brasil)',
+            'Miroslav Klose (Alemanha)',
+            'Cristiano Ronaldo (Portugal)',
+            'Just Fontaine (França)',
+            'Lionel Messi (Argentina)',
+        ],
+        answer: 3
+    },
+    {
+        question: 'Qual o único goleiro a levar o prêmio de Bola de Ouro de uma Copa do Mundo?',
+        options: [
+            'Oliver Kahn (Alemanha)',
+            'Gianluigi Buffon (Itália)',
+            'Thibaut Courtois (Bélgica)',
+            'Iker Casillas (Espanha)',
+            'Edwin van der Sar (Holanda)',
+        ],
+        answer: 0
+    },
+];
---
+++ readme padrão github.md
@@ -0,0 +1,52 @@
+<h1 align="center">Quiz Copa do Mundo :soccer: :trophy:</h1>
+
+<h3>projeto desenvolvido para aplicar conhecimento adquirido em curso, e acrescentar ao portfólio pessoal.</h3>
+<br/><hr><br/>
+
+<h2 align="center">:speech_balloon:Tabela de conteúdos</h2>
+<br/>
+<p align="center">
+    <a href="#sobre">Sobre</a> •
+    <a href="#preview">Pré-Visualização</a> •
+    <a href="#teste">Teste</a> •
+    <a href="#tecnologias">Tecnologias</a> •
+    <a href="#contato">Contato</a> •
+</p>
+<br/><hr><br/>
+<div id="sobre">
+    <h1 align="center">Sobre :pencil2:</h1>
+    <br/>
+    Sistema de perguntas com alternativas para você selecionar e se divertir, criado para fixação de conteúdo e aplicação de conhecimento de javascript.
+</div>
+<br/><hr><br/>
+<div id="preview">
+    <h1>Pré Visualização</h1>
+    <br/>
+    <p align="center">
+    <img src="preview.png" alt="previewdesktop"/>
+    </p>
+</div>
+<div id="teste">
+    <h1>Teste :pencil2:</h1>
+    <p align="center"><a href="https://fernandobardelli.github.io/quiz/" target="_blank">Confira aqui</a></p>
+</div>
+<br/><hr><br/>
+<div id="tecnologias">
+    <h1>Tecnologias :computer:</h1>
+    <img align="center" alt="html5" height="40" width="50" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
+    <img align="center" alt="css3" height="40" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg">
+    <img align="center" alt="css3" height="40" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg">
+</div>
+<br/><hr><br/>
+<div id="contato">
+    <h1>Contato :email:</h1>
+    <p align="center">
+    <a href="https://www.linkedin.com/in/fernando-bardelli-a76926238/" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank"></a>
+    <a href="mailto:fernandofelipebardelli@gmail.com" target="_blank"><img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white">
+    <a href="https://wa.me/5541997075446" target="_blank"><img src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" target="_blank"></a>
+    </p>
+</div>
+
+
+
+
